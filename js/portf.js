0f5c15bcc487cdc998ec701d8b2000cefe084f3e
jamartindev <jamarti94@hotmail.com>
2023-08-04 22:28:25 -0300

    test

--- js/portf.js
+++ js/portf.js
@@ -2,19 +2,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const inputContainers = document.getElementsByClassName('input-form');
 
-    const inputFields = document.getElementsByTagName('input');
+    const inputFields = document.querySelectorAll('input, textarea');
 
     for (let i = 0; i < inputFields.length; i++) {
     
-        inputFields[i].addEventListener('focus', function ()  {
-            inputContainers[i].classList.add('active');
+            inputFields[i].addEventListener('focus', function ()  {
+                inputContainers[i].classList.add('active');
 
-        });
+            });
 
-        inputFields[i].addEventListener('blur', function () {
-            inputContainers[i].classList.remove('active');
-    
-        });
+        if (inputFields[i].value.trim().length === 0) {
+                inputFields[i].addEventListener('blur', function () {
+                    inputContainers[i].classList.remove('active');
+                
+            });
+        } 
     };
 });
 
