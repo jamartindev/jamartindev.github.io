6232e8830eb58f0535abc6477a5564f747c3d0eb
jamartindev <jamarti94@hotmail.com>
2023-08-22 05:29:08 -0300

    navlinks js

--- js/portf.js
+++ js/portf.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     
     const inputContainers = document.getElementsByClassName('input-form');
-    
     const inputFields = document.querySelectorAll('input, textarea');
 
     for (let i = 0; i < inputFields.length; i++) {
@@ -15,8 +14,42 @@ document.addEventListener('DOMContentLoaded', function () {
         function inputControl() {
             if (inputFields[i].value.trim().length === 0) {
                 inputContainers[i].classList.remove('active');
-            }
-        }
-    }
-});
+            } return;
+        };
+    };
+
+    let navLinks = document.getElementsByClassName('a-nav');
+    
+        
+        
+
+        for (let i = 0; i < navLinks.length; i++) {
+            navLinks[i].addEventListener('click', select);
+        };        
+                
+        
+            function select(event) {
+                event.preventDefault();
+                let anchor = event.target.hash;
+                window.location.replace(anchor);
+                
+                window.addEventListener('hashchange', function () {
+                    
+                    if (location.hash === anchor) {
+                        event.target.classList.add('selected');
+                    } else {
+                        event.target.classList.remove('selected');
+                    }
+                });
+            };
+
+}); 
+
+
+
+/*et getAnchor = [];
+
+        for (let i = 0; i < navLinks.length; i++) {
+            getAnchor.push(navLinks[i].getAttribute('href'));
+        } */
 
